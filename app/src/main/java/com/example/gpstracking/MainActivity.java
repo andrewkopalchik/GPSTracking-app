1aacc9cad4e9f8c1190f28ec5692b12b0dae7a69
AndriyKopalchik <andrewkopalchik@gmail.com>
2023-03-29 21:15:37 +0300

    Тепер вже працює

--- app/src/main/java/com/example/gpstracking/MainActivity.java
+++ app/src/main/java/com/example/gpstracking/MainActivity.java
@@ -5,24 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    private GoogleMap myMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
-        assert mapFragment != null;
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
     }
+
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        myMap = googleMap;
+        LatLng sydney = new LatLng(-34,151);
+        myMap.addMarker(new MarkerOptions().position(sydney).title("Sydney"));
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
     }
 }
