6da7be0481817821fea53a9a74fe38d55571d5d9
AndriyKopalchik <andrewkopalchik@gmail.com>
2023-05-02 20:34:18 +0300

    add some code

--- app/src/main/java/com/example/gpstracking/MainActivity.java
+++ app/src/main/java/com/example/gpstracking/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.gpstracking;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
-import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.EditText;
+
+
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,12 +21,28 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+import java.util.Arrays;
 
-    private GoogleMap myMap;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback{
+
+    private GoogleMap mMap;
+    private DatabaseReference databaseReference;
+
+    private LocationListener locationListener;
     private LocationManager locationManager;
-    private LatLng currentLocation;
+    private final long MIN_TIME = 1000;
+    private final long MIN_DIST = 5;
+
+    private EditText editTextLatitude;
+    private EditText editTextLongitude;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,21 +51,38 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        editTextLatitude = findViewById(R.id.editTextTextPersonName);
+        editTextLongitude = findViewById(R.id.editTextTextPersonName2);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Location");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
 
-
-
-        myMap = googleMap;
         LatLng uzhgorod = new LatLng(48.6208, 22.2879);
-        myMap.addMarker(new MarkerOptions().position(uzhgorod).title("Ужгород"));
-        myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(uzhgorod, 15));
-
-
-
+        googleMap.addMarker(new MarkerOptions().position(uzhgorod).title("Ужгород"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(uzhgorod, 15));
 
     }
+
+    public void updateButtonOnClick(View view){
+
+    }
+
+
 }
