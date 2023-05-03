f1cd5d5c735122c0cd37cda7be0438cfdc14ce32
AndriyKopalchik <andrewkopalchik@gmail.com>
2023-05-03 20:42:27 +0300

    Добавив роботу з базою данних, тепер з'явилась можливість відправляти координати в ручну в базу данних в реальному часі

--- app/src/main/java/com/example/gpstracking/MainActivity.java
+++ app/src/main/java/com/example/gpstracking/MainActivity.java
@@ -14,7 +14,6 @@ import android.view.View;
 import android.widget.EditText;
 
 
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -30,7 +29,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.Arrays;
 
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback{
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private DatabaseReference databaseReference;
@@ -58,7 +57,28 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         databaseReference = FirebaseDatabase.getInstance().getReference("Location");
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                try{
+
+                    String databaseLatitudeString = dataSnapshot.child("latitude").getValue().toString().substring(1,dataSnapshot.child("latitude").getValue().toString().length()-1);
+                    String databaseLongitudeString = dataSnapshot.child("longitude").getValue().toString().substring(1,dataSnapshot.child("longitude").getValue().toString().length()-1);
+
+                    String[] stringLat =  databaseLatitudeString.split(", ");
+                    Arrays.sort(stringLat);
+                    String latitude = stringLat[stringLat.length-1].split("=")[1];
+
+                    String[] stringLong = databaseLongitudeString.split(", ");
+                    Arrays.sort(stringLong);
+                    String longitude = stringLong[stringLong.length-1].split("=")[1];
+
+                    LatLng latLng = new LatLng(Double.parseDouble(latitude), Double.parseDouble(longitude));
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(latitude + " , " + longitude));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
 
             }
 
@@ -74,13 +94,53 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
 
+        mMap = googleMap;
+        /*
         LatLng uzhgorod = new LatLng(48.6208, 22.2879);
         googleMap.addMarker(new MarkerOptions().position(uzhgorod).title("Ужгород"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(uzhgorod, 15));
 
+         */
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                try {
+                    editTextLatitude.setText(Double.toString(location.getLatitude()));
+                    editTextLongitude.setText(Double.toString(location.getLongitude()));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        try {
+
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DIST, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DIST, locationListener);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void updateButtonOnClick(View view){
+
+        databaseReference.child("latitude").push().setValue(editTextLatitude.getText().toString());
+        databaseReference.child("longitude").push().setValue(editTextLongitude.getText().toString());
+
 
     }
 
